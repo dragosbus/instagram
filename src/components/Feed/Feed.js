22cf7810b19e0aaa30eed01f20812b66ffae3eb8
dragosbus <dragosbusuioc94@gmail.com>
2018-09-23 15:52:11 +0200

    styled the feed page

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,36 +1,57 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-import {getFollowingPostsMiddleware, getUserDataMiddleware} from '../../actionCreators/actions';
-import {
-  firebase
-} from '../../firebase/firebase';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { getFollowingPostsMiddleware, getUserDataMiddleware } from '../../actionCreators/actions';
+import { firebase } from '../../firebase/firebase';
+import { Link } from 'react-router-dom';
+import { MdFavoriteBorder, MdChatBubbleOutline } from 'react-icons/md';
+
+import './Feed.css';
 
 class Feed extends Component {
   state = {
     posts: []
-  }
-  
+  };
+
   componentDidMount() {
     this.props.getPosts(this.props.userId);
   }
 
   render() {
-    const {followingPosts} = this.props;
-    console.log(followingPosts)
-    return(
+    const { followingPosts } = this.props;
+    console.log(followingPosts);
+    return (
       <div className="feed">
         <ul>
-          {
-            followingPosts.map(post=>{
-              return <li key={post.description}>
+          {followingPosts.map(post => {
+            return (
+              <li key={`${post.description}-${post.userId}`}>
                 <div className="header-post">
-                  <img src={post.profile_photo}/>
-                  <h4>{post.username}</h4>
+                  <Link to={`/${post.userId}`}>
+                    <img src={post.profile_photo} />
+                    <h4>{post.username}</h4>
+                  </Link>
+                </div>
+                <div className="main-post">
+                  <img src={post.photo} />
+                  <div className="actions">
+                    <button>
+                      <MdFavoriteBorder />
+                    </button>
+                    <button>
+                      <MdChatBubbleOutline />
+                    </button>
+                  </div>
+                  <p>{post.likes} Likes</p>
+                  <p>
+                    <span>{post.username}:</span>
+                    {post.description}
+                  </p>
+                  <p>Created at time ago</p>
                 </div>
               </li>
-            })
-          }
+            );
+          })}
         </ul>
       </div>
     );
@@ -41,8 +62,15 @@ const mapStateToProps = state => ({
   followingPosts: state.followingPosts
 });
 
-const mapDispatchToProps = dispatch => bindActionCreators({
-  getPosts: getFollowingPostsMiddleware
-}, dispatch);
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      getPosts: getFollowingPostsMiddleware
+    },
+    dispatch
+  );
 
-export default connect(mapStateToProps, mapDispatchToProps)(Feed);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Feed);
