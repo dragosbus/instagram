8112a4391d871b5bc3d98700da61ee7c23428fab
dragosbus <dragosbusuioc94@gmail.com>
2018-09-26 18:21:12 +0200

    solved typos in Profile.js

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { db } from '../../firebase/firebase';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { getUserDataMiddleware, isFollowMiddleware } from '../../actionCreators/actions';
