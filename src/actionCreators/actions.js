ba4cfdb0451161c08464510caec7b9713668c5b8
dragosbus <dragosbusuioc94@gmail.com>
2018-10-12 13:27:50 +0200

    init created 404 page

--- src/actionCreators/actions.js
+++ src/actionCreators/actions.js
@@ -128,14 +128,14 @@ export const getPostsForFeed = userId => dispatch => {
       let currentUser = res && index <= Object.values(res).length - 1 ? Object.values(res)[index].id : null;
 
       if (currentUser) {
-        getDataFromFirebase(`posts/${currentUser}`).then(res => {
-          if (!res) {
+        getDataFromFirebase(`posts/${currentUser}`).then(post => {
+          if (!post) {
             dispatch(getFeedError());
             //if the current user dosnt have posts, we should get the next user by call recursive getPost function
             getPost(index + 1);
           } else {
             //create the post and dispatch it if the current user has posts
-            const postsFetchedArr = Object.values(res);
+            const postsFetchedArr = Object.values(post);
             let currentPost = postsFetchedArr[postsFetchedArr.length - 1];
 
             getDataFromFirebase(`users/${currentPost.userId}`).then(user => {
