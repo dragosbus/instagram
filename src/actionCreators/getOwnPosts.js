a76853f3d75c645b36a7f4551bf9d055141935f6
dragosbus <dragosbusuioc94@gmail.com>
2018-10-13 19:07:13 +0200

    refactored getOwnPostsMiddleware

--- src/actionCreators/getOwnPosts.js
+++ src/actionCreators/getOwnPosts.js
@@ -7,21 +7,18 @@ import {
   getDataFromFirebase
 } from '../utils/firebaseHandlers';
 
-const getPosts = posts => ({
+export const getPosts = posts => ({
   type: ActionTypes.GET_USER_POSTS,
   payload: posts
 });
 
 export const getPostsMiddleware = userId => dispatch => {
-  //is called every time the user route is changing to other user
-  //so it need to be empty, not to contain the posts of the previous user
-  let userPosts = [];
-  db.ref(`posts/${userId}`).on('value', s => {
-    if (s.val()) {
-      //if the selected user dosnt have posts, it return an error
-      //thats why we need to check
-      userPosts = [...Object.values(s.val())];
-    }
-    dispatch(getPosts(userPosts));
-  });
-};
+    //is called every time the user route is changing to other user
+    //so it need to be empty, not to contain the posts of the previous user, so we dispatch an empty array synchronous and asynchronous the posts for the current user
+    let userPosts = [];
+    dispatch(getPosts([]));
+    getDataFromFirebase(`posts/${userId}`).then(res => {
+        userPosts = res ? [...Object.values(res)] : [];
+        userPosts.length ? dispatch(getPosts(userPosts)) : dispatch(getPosts([]));
+        });
+    };
