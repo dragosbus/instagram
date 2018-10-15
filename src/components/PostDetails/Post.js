ba9cd9f73c2a1f86b013048dd93a75715f324e25
dragosbus <dragosbusuioc94@gmail.com>
2018-10-15 15:31:47 +0200

    for mantainig style of the like button if the post is liked, check if the current user liked the post in the componentWillReceiveProps lifecycle method of the post details component

--- src/components/PostDetails/Post.js
+++ src/components/PostDetails/Post.js
@@ -4,37 +4,39 @@ import { MdFavoriteBorder, MdChatBubbleOutline } from 'react-icons/md';
 import { Link } from 'react-router-dom';
 
 class PostDetails extends React.Component {
-  componentDidUpdate() {
-    //this is bad.It block the page
-    this.props.checkLikePost(
-      this.props.postId,
-      this.props.userId,
-      this.props.userConnected
-    );
+  componentWillReceiveProps(props) {
+    if (props.showDetailsPost) {
+      this.props.checkLikePost(this.props.postId, this.props.userId, this.props.userConnected);
+    }
   }
   render() {
     let data = this.props.data;
     return (
       <div className="overlay" style={{ display: this.props.showDetailsPost ? 'block' : 'none' }}>
-        <button className="close-post-modal" onClick={this.props.toggleModal}>X</button>
+        <button className="close-post-modal" onClick={this.props.toggleModal}>
+          X
+        </button>
         <div className="post-details">
           <div className="header">
-            <Link to={`/${this.props.userId}`} onClick={()=>{
-              this.props.hideModal();
-            }}>
-              <img src={data.profile_picture} alt='profile user'/>
+            <Link
+              to={`/${this.props.userId}`}
+              onClick={() => {
+                this.props.hideModal();
+              }}
+            >
+              <img src={data.profile_picture} alt="profile user" />
               <p>{data.username}</p>
             </Link>
           </div>
           <div className="post-image">
-            <img src={this.props.postImg} alt="post detail"/>
+            <img src={this.props.postImg} alt="post detail" />
           </div>
           <div className="detail">
             {/* <ul>
               {data.comments.map(comment=>)}
             </ul> */}
             <div className="actions">
-              <button onClick={this.props.likePost} style={{background: this.props.isLiked ? 'red' : 'none'}}>
+              <button onClick={this.props.likePost} style={{ background: this.props.isLiked ? 'red' : 'none' }}>
                 <MdFavoriteBorder />
               </button>
               <button>
