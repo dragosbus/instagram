b1587e61f79b25820c9138649a129746ff332702
dragosbus <dragosbusuioc94@gmail.com>
2018-10-18 15:33:26 +0200

    diapatch login-in-succes if exist and user in indexdb saved

--- src/store.js
+++ src/store.js
@@ -1,7 +1,17 @@
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
+import {auth} from './firebase/firebase';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+auth.onAuthStateChanged(function (user) {
+  if (user) {
+    store.dispatch({type:'LOG_IN_SUCCESS', payload: {id: user.uid}})
+  } else {
+    // User is signed out.
+    // ...
+  }
+});
+
+const store = createStore(rootReducer,applyMiddleware(thunk));
 
 export default store;
